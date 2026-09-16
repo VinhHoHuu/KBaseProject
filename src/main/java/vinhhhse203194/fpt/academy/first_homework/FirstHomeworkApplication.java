0e9513c5804b84ffa895f9baeb5090b04ae066ae
VinhHoHuu <thuydungpham8419@gmail.com>
2026-09-16 13:15:13 +0700

    Initial commit: Base Spring Boot project with Docker config for KBase

---
+++ src/main/java/vinhhhse203194/fpt/academy/first_homework/FirstHomeworkApplication.java
@@ -0,0 +1,13 @@
+package vinhhhse203194.fpt.academy.first_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstHomeworkApplication.class, args);
+	}
+
+}
