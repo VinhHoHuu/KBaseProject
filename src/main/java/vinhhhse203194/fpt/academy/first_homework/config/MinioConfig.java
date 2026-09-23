7c41c59c818eef74f1f86f054e990865df0d254c
VinhHoHuu <thuydungpham8419@gmail.com>
2026-09-23 10:01:23 +0700

    fix: Patch MinioClient to support Supabase S3 path endpoints

--- src/main/java/vinhhhse203194/fpt/academy/first_homework/config/MinioConfig.java
+++ src/main/java/vinhhhse203194/fpt/academy/first_homework/config/MinioConfig.java
@@ -20,10 +20,35 @@ public class MinioConfig {
 
     // Khởi tạo Bean MinioClient để Spring quản lý
     @Bean
-    public MinioClient minioClient() {
+    public MinioClient minioClient() throws Exception {
+        String endpoint = url;
+        String basePath = "";
+        
+        java.net.URL parsedUrl = new java.net.URL(url);
+        if (parsedUrl.getPath() != null && !parsedUrl.getPath().isEmpty() && !parsedUrl.getPath().equals("/")) {
+            basePath = parsedUrl.getPath();
+            endpoint = parsedUrl.getProtocol() + "://" + parsedUrl.getHost() + (parsedUrl.getPort() == -1 ? "" : ":" + parsedUrl.getPort());
+        }
+
+        final String pathToInject = basePath;
+
+        okhttp3.OkHttpClient httpClient = new okhttp3.OkHttpClient.Builder()
+                .addInterceptor(chain -> {
+                    okhttp3.Request request = chain.request();
+                    if (!pathToInject.isEmpty()) {
+                        okhttp3.HttpUrl newUrl = request.url().newBuilder()
+                                .encodedPath(pathToInject + request.url().encodedPath())
+                                .build();
+                        request = request.newBuilder().url(newUrl).build();
+                    }
+                    return chain.proceed(request);
+                })
+                .build();
+
         return MinioClient.builder()
-                .endpoint(url)
+                .endpoint(endpoint)
                 .credentials(accessKey, secretKey)
+                .httpClient(httpClient)
                 .build();
     }
 }
